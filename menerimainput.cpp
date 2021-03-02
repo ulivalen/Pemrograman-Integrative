f3eb462d330db59f6f69f8fba9a0203ffb689b1b
ulivalen <56582351+ulivalen@users.noreply.github.com>
2021-03-02 11:16:14 +0700

    Create menerimainput.cpp

---
+++ menerimainput.cpp
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main()
+{
+  int x;
+  int y;
+  printf("masukkkan nilai x: ");
+  scanf ("%d" , &x);
+   printf("masukkkan nilai y: ");
+  scanf ("%d" , &y);
+  
+  printf("x + y = %d" , x + y);
+  return 0;
+}
+  
