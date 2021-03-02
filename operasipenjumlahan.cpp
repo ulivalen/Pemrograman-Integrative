047a0079206966b295b79fc203c4440d3855e4b3
ulivalen <56582351+ulivalen@users.noreply.github.com>
2021-03-02 10:49:36 +0700

    Create operasipenjumlahan.cpp

---
+++ operasipenjumlahan.cpp
@@ -0,0 +1,21 @@
+#include
+
+#include
+
+int jumlah(int a, int b);
+
+void main(){
+
+clrscr();
+
+cout << “Hasil penjumlahan : “ << jumlah(5,8);
+
+getch();}
+
+int jumlah(int a, int b)
+
+{
+
+return(a + b);
+
+}
